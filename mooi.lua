a67fd0555aafd553b7a82f407bf27749029b0e7f
HazeWasTaken <56366650+HazeWasTaken@users.noreply.github.com>
2020-12-14 23:32:46 +0200

    Update mooi.lua

--- mooi.lua
+++ mooi.lua
@@ -325,4 +325,220 @@ function library:CreateFrame()
     end
     return UtilsLib
 end
+-- Luazifier v0.1
+-- Settings: Mode > Table | Custom Name > false | Security > Off
+
+-- Setup:
+Luazifier = {} 
+setmetatable(Luazifier, {__metatable = "Don' t touch my table P-Pervert!"})
+
+-- Instances:
+Luazifier.ScreenGui1 = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"))
+Luazifier.ImageLabel1 = Instance.new("ImageLabel", Luazifier.ScreenGui1)
+Luazifier.TextLabel1 = Instance.new("TextLabel", Luazifier.ImageLabel1)
+Luazifier.ImageLabel2 = Instance.new("ImageLabel", Luazifier.ImageLabel1)
+Luazifier.TextLabel2 = Instance.new("TextLabel", Luazifier.ImageLabel2)
+Luazifier.TextButton1 = Instance.new("TextButton", Luazifier.ImageLabel1)
+Luazifier.ImageLabel3 = Instance.new("ImageLabel", Luazifier.TextButton1)
+
+-- Properties:
+Luazifier.ScreenGui1.DisplayOrder = 0
+Luazifier.ScreenGui1.Enabled = true
+Luazifier.ScreenGui1.IgnoreGuiInset = false
+Luazifier.ScreenGui1.Name = "ScreenGui"
+Luazifier.ScreenGui1.ResetOnSpawn = true
+Luazifier.ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Global
+Luazifier.ScreenGui1.Archivable = true
+Luazifier.ScreenGui1.AutoLocalize = true
+
+
+Luazifier.ImageLabel1.Active = false
+Luazifier.ImageLabel1.AnchorPoint = Vector2.new(0, 0)
+Luazifier.ImageLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.ImageLabel1.BackgroundTransparency = 1
+Luazifier.ImageLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.ImageLabel1.BorderSizePixel = 1
+Luazifier.ImageLabel1.LayoutOrder = 0
+Luazifier.ImageLabel1.Name = "Frame"
+Luazifier.ImageLabel1.Position = UDim2.new(0.31953522562981, 0, 0.33906632661819, 0)
+Luazifier.ImageLabel1.Rotation = 0
+Luazifier.ImageLabel1.Selectable = false
+Luazifier.ImageLabel1.Size = UDim2.new(0, 468, 0, 246)
+Luazifier.ImageLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.ImageLabel1.Visible = true
+Luazifier.ImageLabel1.ZIndex = 1
+Luazifier.ImageLabel1.Archivable = true
+Luazifier.ImageLabel1.ClipsDescendants = false
+Luazifier.ImageLabel1.AutoLocalize = true
+Luazifier.ImageLabel1.Image = "rbxassetid://3570695787"
+Luazifier.ImageLabel1.ImageColor3 = Color3.new(0.129412, 0.129412, 0.129412)
+Luazifier.ImageLabel1.ImageRectOffset = Vector2.new(0, 0)
+Luazifier.ImageLabel1.ImageRectSize = Vector2.new(0, 0)
+Luazifier.ImageLabel1.ImageTransparency = 0
+Luazifier.ImageLabel1.ScaleType = Enum.ScaleType.Slice
+Luazifier.ImageLabel1.SliceCenter  = Rect.new(100, 100, 100, 100)
+Luazifier.ImageLabel1.SliceScale = 0.03999999910593
+
+
+Luazifier.TextLabel1.Active = false
+Luazifier.TextLabel1.AnchorPoint = Vector2.new(0, 0)
+Luazifier.TextLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel1.BackgroundTransparency = 1
+Luazifier.TextLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.TextLabel1.BorderSizePixel = 1
+Luazifier.TextLabel1.LayoutOrder = 0
+Luazifier.TextLabel1.Name = "TextLabel"
+Luazifier.TextLabel1.Position = UDim2.new(0.28632479906082, 0, 0.044715445488691, 0)
+Luazifier.TextLabel1.Rotation = 0
+Luazifier.TextLabel1.Selectable = false
+Luazifier.TextLabel1.Size = UDim2.new(0, 200, 0, 50)
+Luazifier.TextLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.TextLabel1.Visible = true
+Luazifier.TextLabel1.ZIndex = 1
+Luazifier.TextLabel1.Archivable = true
+Luazifier.TextLabel1.ClipsDescendants = false
+Luazifier.TextLabel1.AutoLocalize = true
+Luazifier.TextLabel1.Font = Enum.Font.GothamSemibold
+Luazifier.TextLabel1.Text = "PayPal"
+Luazifier.TextLabel1.TextColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel1.TextScaled = false
+Luazifier.TextLabel1.TextSize = 30
+Luazifier.TextLabel1.TextStrokeColor3 = Color3.new(0, 0, 0)
+Luazifier.TextLabel1.TextStrokeTransparency = 1
+Luazifier.TextLabel1.TextTransparency = 0
+Luazifier.TextLabel1.TextTruncate = Enum.TextTruncate.None
+Luazifier.TextLabel1.TextWrapped = false
+Luazifier.TextLabel1.TextXAlignment = Enum.TextXAlignment.Center
+Luazifier.TextLabel1.TextYAlignment = Enum.TextYAlignment.Center
+
+
+Luazifier.ImageLabel2.Active = false
+Luazifier.ImageLabel2.AnchorPoint = Vector2.new(0, 0)
+Luazifier.ImageLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.ImageLabel2.BackgroundTransparency = 1
+Luazifier.ImageLabel2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.ImageLabel2.BorderSizePixel = 1
+Luazifier.ImageLabel2.LayoutOrder = 0
+Luazifier.ImageLabel2.Name = "Frame"
+Luazifier.ImageLabel2.Position = UDim2.new(0.012820512987673, 0, 0.32926830649376, 0)
+Luazifier.ImageLabel2.Rotation = 0
+Luazifier.ImageLabel2.Selectable = false
+Luazifier.ImageLabel2.Size = UDim2.new(0, 456, 0, 158)
+Luazifier.ImageLabel2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.ImageLabel2.Visible = true
+Luazifier.ImageLabel2.ZIndex = 1
+Luazifier.ImageLabel2.Archivable = true
+Luazifier.ImageLabel2.ClipsDescendants = false
+Luazifier.ImageLabel2.AutoLocalize = true
+Luazifier.ImageLabel2.Image = "rbxassetid://3570695787"
+Luazifier.ImageLabel2.ImageColor3 = Color3.new(0.290196, 0.290196, 0.290196)
+Luazifier.ImageLabel2.ImageRectOffset = Vector2.new(0, 0)
+Luazifier.ImageLabel2.ImageRectSize = Vector2.new(0, 0)
+Luazifier.ImageLabel2.ImageTransparency = 0
+Luazifier.ImageLabel2.ScaleType = Enum.ScaleType.Slice
+Luazifier.ImageLabel2.SliceCenter  = Rect.new(100, 100, 100, 100)
+Luazifier.ImageLabel2.SliceScale = 0.03999999910593
+
+
+Luazifier.TextLabel2.Active = false
+Luazifier.TextLabel2.AnchorPoint = Vector2.new(0, 0)
+Luazifier.TextLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel2.BackgroundTransparency = 1
+Luazifier.TextLabel2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.TextLabel2.BorderSizePixel = 1
+Luazifier.TextLabel2.LayoutOrder = 0
+Luazifier.TextLabel2.Name = "TextLabel"
+Luazifier.TextLabel2.Position = UDim2.new(0.015350877307355, 0, 0.038961037993431, 0)
+Luazifier.TextLabel2.Rotation = 0
+Luazifier.TextLabel2.Selectable = false
+Luazifier.TextLabel2.Size = UDim2.new(0, 443, 0, 140)
+Luazifier.TextLabel2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.TextLabel2.Visible = true
+Luazifier.TextLabel2.ZIndex = 1
+Luazifier.TextLabel2.Archivable = true
+Luazifier.TextLabel2.ClipsDescendants = false
+Luazifier.TextLabel2.AutoLocalize = true
+Luazifier.TextLabel2.Font = Enum.Font.GothamSemibold
+Luazifier.TextLabel2.Text = "Hi i have recently decided to take a break from discord.".."\n" ..  "So any updates will apear here"
+Luazifier.TextLabel2.TextColor3 = Color3.new(1, 1, 1)
+Luazifier.TextLabel2.TextScaled = false
+Luazifier.TextLabel2.TextSize = 14
+Luazifier.TextLabel2.TextStrokeColor3 = Color3.new(0, 0, 0)
+Luazifier.TextLabel2.TextStrokeTransparency = 1
+Luazifier.TextLabel2.TextTransparency = 0
+Luazifier.TextLabel2.TextTruncate = Enum.TextTruncate.None
+Luazifier.TextLabel2.TextWrapped = false
+Luazifier.TextLabel2.TextXAlignment = Enum.TextXAlignment.Left
+Luazifier.TextLabel2.TextYAlignment = Enum.TextYAlignment.Top
+
+
+Luazifier.TextButton1.Active = true
+Luazifier.TextButton1.AnchorPoint = Vector2.new(0, 0)
+Luazifier.TextButton1.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+Luazifier.TextButton1.BackgroundTransparency = 1
+Luazifier.TextButton1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.TextButton1.BorderSizePixel = 0
+Luazifier.TextButton1.LayoutOrder = 0
+Luazifier.TextButton1.Modal = false
+Luazifier.TextButton1.Name = "TextButton"
+Luazifier.TextButton1.Position = UDim2.new(0.94394171237946, 0, 0.04363676905632, 0)
+Luazifier.TextButton1.Rotation = 0
+Luazifier.TextButton1.Selectable = true
+Luazifier.TextButton1.Selected = false
+Luazifier.TextButton1.Size = UDim2.new(0, 20, 0, 20)
+Luazifier.TextButton1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.TextButton1.Style = Enum.ButtonStyle.Custom
+Luazifier.TextButton1.Visible = true
+Luazifier.TextButton1.ZIndex = 2
+Luazifier.TextButton1.Archivable = true
+Luazifier.TextButton1.ClipsDescendants = false
+Luazifier.TextButton1.AutoLocalize = true
+Luazifier.TextButton1.Font = Enum.Font.GothamSemibold
+Luazifier.TextButton1.Text = "X"
+Luazifier.TextButton1.TextColor3 = Color3.new(1, 1, 1)
+Luazifier.TextButton1.TextScaled = false
+Luazifier.TextButton1.TextSize = 14
+Luazifier.TextButton1.TextStrokeColor3 = Color3.new(0, 0, 0)
+Luazifier.TextButton1.TextStrokeTransparency = 1
+Luazifier.TextButton1.TextTransparency = 0
+Luazifier.TextButton1.TextTruncate = Enum.TextTruncate.None
+Luazifier.TextButton1.TextWrapped = false
+Luazifier.TextButton1.TextXAlignment = Enum.TextXAlignment.Center
+Luazifier.TextButton1.TextYAlignment = Enum.TextYAlignment.Center
+Luazifier.TextButton1.MouseButton1Click:Connect(function()
+	Luazifier.ScreenGui1:Destroy()
+end)
+
+Luazifier.ImageLabel3.Active = true
+Luazifier.ImageLabel3.AnchorPoint = Vector2.new(0.5, 0.5)
+Luazifier.ImageLabel3.BackgroundColor3 = Color3.new(1, 1, 1)
+Luazifier.ImageLabel3.BackgroundTransparency = 1
+Luazifier.ImageLabel3.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+Luazifier.ImageLabel3.BorderSizePixel = 1
+Luazifier.ImageLabel3.LayoutOrder = 0
+Luazifier.ImageLabel3.Name = "TextButton_Roundify_4px"
+Luazifier.ImageLabel3.Position = UDim2.new(0.5, 0, 0.5, 0)
+Luazifier.ImageLabel3.Rotation = 0
+Luazifier.ImageLabel3.Selectable = true
+Luazifier.ImageLabel3.Size = UDim2.new(1, 0, 1, 0)
+Luazifier.ImageLabel3.SizeConstraint = Enum.SizeConstraint.RelativeXY
+Luazifier.ImageLabel3.Visible = true
+Luazifier.ImageLabel3.ZIndex = 1
+Luazifier.ImageLabel3.Archivable = true
+Luazifier.ImageLabel3.ClipsDescendants = false
+Luazifier.ImageLabel3.AutoLocalize = true
+Luazifier.ImageLabel3.Image = "rbxassetid://3570695787"
+Luazifier.ImageLabel3.ImageColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+Luazifier.ImageLabel3.ImageRectOffset = Vector2.new(0, 0)
+Luazifier.ImageLabel3.ImageRectSize = Vector2.new(0, 0)
+Luazifier.ImageLabel3.ImageTransparency = 0
+Luazifier.ImageLabel3.ScaleType = Enum.ScaleType.Slice
+Luazifier.ImageLabel3.SliceCenter  = Rect.new(100, 100, 100, 100)
+Luazifier.ImageLabel3.SliceScale = 0.03999999910593
+
+
+-- Scripts:
+
+
+
 return library
